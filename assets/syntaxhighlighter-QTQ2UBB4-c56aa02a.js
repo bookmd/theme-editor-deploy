7128640171578c215f3fb31470f36491e524db4f
Ori Ziskind <9935756+orizis@users.noreply.github.com>
2024-12-11 12:36:36 +0000

    Update new version from static build

---
+++ assets/syntaxhighlighter-QTQ2UBB4-c56aa02a.js
@@ -0,0 +1,2 @@
+import{S as n,c as d,s as f}from"./DocsRenderer-7FRJXR4N-cbf27a31.js";import"./iframe-a8bcc5e9.js";import"../sb-preview/runtime.mjs";import"./index-5e0a97bb.js";import"./_commonjsHelpers-c38d1a10.js";import"./_getPrototype-0ca3a8da.js";import"./index-d475d2ea.js";import"./index-19cb0178.js";import"./index-32ac9e7b.js";import"./preview-4475f18b.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
+//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-c56aa02a.js.map
